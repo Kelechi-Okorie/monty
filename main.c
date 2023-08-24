fdb166fb20c13a02883351826625412ef6b8df01
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-24 20:46:06 +0000

    Hot fix

--- main.c
+++ main.c
@@ -27,6 +27,7 @@ int main(int argc, char **argv)
 	line_len = nread = 0;
 	lineptr = opcode = argument = NULL;
 	line_number = 0;
+	global_var.exit_status = 0;
 
 	if (argc != 2)
 	{
@@ -58,6 +59,7 @@ int main(int argc, char **argv)
 		if (f)
 		{
 			f(&head, line_number);
+			break;
 		}
 		else
 		{
@@ -67,6 +69,10 @@ int main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 	}
+
+	fclose(stream);
+	free_stack(&head);
+	free(lineptr);
 
 	return (0);
 }
